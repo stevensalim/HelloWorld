86bd74dd9dc8135e34802c4a69329da41228362c
Student <Student@LAB-101-31>
2018-01-16 18:17:05 +0700

    Revert "Show Hello World"
    This reverts commit 502e89b4a6032f190168adba5f50cbb8e0c3022a.

--- src/hellogit/HelloGit.java
+++ src/hellogit/HelloGit.java
@@ -5,18 +5,16 @@
  */
 package hellogit;
 
-import java.util.Scanner;
-
-
+/**
+ *
+ * @author Student
+ */
 public class HelloGit {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Input your name: ");
-        String name = input.nextLine();
         System.out.println("Hello World");
     }
     
