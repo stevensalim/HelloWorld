502e89b4a6032f190168adba5f50cbb8e0c3022a
Student <Student@LAB-101-31>
2018-01-16 18:15:39 +0700

    Show Hello World

--- src/hellogit/HelloGit.java
+++ src/hellogit/HelloGit.java
@@ -5,16 +5,18 @@
  */
 package hellogit;
 
-/**
- *
- * @author Student
- */
+import java.util.Scanner;
+
+
 public class HelloGit {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Input your name: ");
+        String name = input.nextLine();
         System.out.println("Hello World");
     }
     
